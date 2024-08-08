27bbd17805bbe09ba199e920c7040a5072b9b660
agilistikmal <agilistikmal3@gmail.com>
2024-08-08 21:33:50 +0700

    Bot Status

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -25,4 +25,6 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+
+	discordService.Client.Close()
 }
--- internal/app/discord/service.go
+++ internal/app/discord/service.go
@@ -1,6 +1,7 @@
 package discord
 
 import (
+	"log"
 	"os"
 
 	"github.com/bwmarrin/discordgo"
@@ -13,8 +14,17 @@ type Service struct {
 func NewService() *Service {
 	client, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+
+	client.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = client.Open()
+	if err != nil {
+		log.Fatal("error opening connection,", err)
+	}
+
+	client.UpdateListeningStatus("JKT48")
 
 	return &Service{
 		Client: client,
